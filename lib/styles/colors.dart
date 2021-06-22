6d0f119d7ad5a9ddd13b919d793b54509acc30ba
adtoba <adetoba53@gmail.com>
2021-06-22 11:42:04 +0100

    configure app styles

---
+++ lib/styles/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class GColors {
+  static Color white = Colors.white;
+  static Color black = Colors.black;
+  static Color blue = Colors.blue;
+  static Color grey = Colors.grey;
+}
---
+++ lib/styles/textstyle.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:gemography_mobile_challenge/styles/colors.dart';
+
+class GTextStyle {
+  static final bold =
+      TextStyle(color: GColors.black, fontWeight: FontWeight.w800);
+
+  static final normal = TextStyle(
+    color: GColors.black,
+  );
+}
